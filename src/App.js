d8408e9452e1fe6cfbffbc7ae7291619b0ca30d6
Kesrinath <knmsurajmishra001@gmail.com>
2023-09-26 18:34:23 +0530

    final project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import Accounts from "./pages/Accounts";
+import Dashboard from "./pages/Dashboard";
+import Products from "./pages/Products";
+import Profilepage from "./pages/Profilepage";
+import Reports from "./pages/Reports";
+import Settings from "./pages/Setting";
+import Loginpage from "./pages/loginpage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <div className="App">
+          <Routes>
+            <Route exact path="/" element={<Loginpage />} />
+            <Route exact path="/pages/Dashboard" element={<Dashboard />} />
+            <Route exact path="/pages/Reports" element={<Reports />} />
+            <Route exact path="/pages/Accounts" element={<Accounts />} />
+            <Route exact path="/pages/Products" element={<Products />} />
+            <Route exact path="/pages/Settings" element={<Settings />} />
+            <Route exact path="/pages/Profilepage" element={<Profilepage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
